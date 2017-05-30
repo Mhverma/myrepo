19bbd5cee121367ee2d87a228ae88567142a4145
Manoj <manojkumarhverma@gmail.com>
2017-05-30 22:07:14 +0530

    added welcome msg

--- HelloGit/src/com/manoj/git/example/HelloGit.java
+++ HelloGit/src/com/manoj/git/example/HelloGit.java
@@ -3,6 +3,7 @@ package com.manoj.git.example;
 public class HelloGit {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to git");
 		System.out.println("This is my first Git hub project");
 		
 	}
